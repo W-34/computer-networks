7397d2bbefaad2e92af5060dcf62aa7c5107cc57
W-34 <20281022@bjtu.edu.cn>
2023-05-08 12:12:25 +0800

    fix

--- project2/src/EmailClient.java
+++ project2/src/EmailClient.java
@@ -1,19 +1,20 @@
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.Socket;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
+import com.sun.mail.smtp.*;
 public class EmailClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // 加载配置文件
         Properties properties = new Properties();
-        InputStream inputStream = EmailClient.class.getClassLoader().getResourceAsStream("../mail.properties");
+        FileInputStream inputStream = new FileInputStream("D:/.vscode/java2023/mail.properties");
         try {
             properties.load(inputStream);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
         // 创建会话对象，用于和SMTP服务器通信
         Session session = Session.getInstance(properties, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -24,13 +25,16 @@ public class EmailClient {
         try {
             // 创建邮件消息对象
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(properties.getProperty("mail.user")));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("收件人的邮箱地址"));
-            message.setSubject("邮件主题");
-            message.setText("邮件正文");
+            message.setFrom(new InternetAddress(properties.getProperty("mail.user")+"@qq.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("20281022@bjtu.edu.cn"));
+            message.setSubject("test");
+            message.setText("smtp client test");
 
             // 发送邮件消息
-            Transport.send(message);
+            SMTPTransport t = (SMTPTransport)session.getTransport("smtp");
+            Socket socket = new Socket("smtp.qq.com", 587);
+            t.connect(socket);
+            t.sendMessage(message, message.getAllRecipients());
             System.out.println("邮件发送成功！");
         } catch (MessagingException e) {
             throw new RuntimeException(e);
